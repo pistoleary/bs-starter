1af6da0dcbfeba5d499a850bbb8e533041b0a96c
Peter O'Leary <pistoleary@gmail.com>
2016-07-02 15:24:59 +0800

    Add relevant info, improve styling, and add tooltips to contact icons.

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('[data-toggle="email-tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="wechat-tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="loc-tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="skype-tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="heart-tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="phone-tooltip"]').tooltip()
+})
